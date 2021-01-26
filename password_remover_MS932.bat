f6a8cd389601ddb904e7650225f89cd94743322a
akihi <akihi0319@gmail.com>
2021-01-26 21:45:33 +0900

    初回コミット

---
+++ password_remover_MS932.bat
@@ -0,0 +1,27 @@
+echo off
+
+rem 開封時パスワード
+set password="123456"
+
+rem 入力元フォルダ
+mkdir input
+rem 出力先フォルダ
+mkdir output
+
+cd input
+
+rem 【①】拡張子がpdfのファイルを全件ループ
+for %%F in (*.pdf) do (
+
+  rem 【②】パスワード解除
+  pdftk %%F input_pw %password% output ..\output\%%F
+
+  rem 【メモ】パスワード設定
+  rem pdftk %%F output pass%%F user_pw %password%
+)
+
+cd ..
+
+echo on
+
+pause
